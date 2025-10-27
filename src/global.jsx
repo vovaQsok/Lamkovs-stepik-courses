194cbd5ba341058855b5bf92467d5e91e4f71a36
vovaQsok <qraqra88@gmail.com>
2025-10-27 16:13:42 +0300

    step 2.5 (BurgerButton)

--- src/global.jsx
+++ src/global.jsx
@@ -16,7 +16,7 @@ export default function (props) {
         <>
             <Head htmlAttributes={{lang: "en"}}>
                 <title>Stream Vibe | {title}</title>
-                <script src="/src/main.js" type="module"/>
+                <script src="/src/main.js" type="module" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
