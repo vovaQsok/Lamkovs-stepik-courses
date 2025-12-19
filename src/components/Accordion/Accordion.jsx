8d9a20074c56938f69b186eb55239d594bb50cd9
vovaQsok <qraqra88@gmail.com>
2025-12-19 21:46:48 +0300

    step(s) 3.7 - 3.8 (section: Questions | accordion)

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,43 @@
+import './Accordion.scss'
+import classNames from 'classnames'
+
+const Accordion = (props) => {
+    const {
+        className,
+        title,
+        id,
+        name,
+        isOpen,
+        children,
+    } = props
+
+    return (
+        <div className= 'accordion'
+        >
+            <details
+                className="accordion__details"
+                name={name}
+                open={isOpen}
+            >
+                <summary className="accordion__summary">
+                    <h3 className="accordion__title h5">
+                        <span role="term" aria-details={id}>{title}</span>
+                    </h3>
+                </summary>
+            </details>
+            <div
+                className="accordion__content"
+                id={id}
+                role="defenition"
+            >
+                <div className="accordion__content-inner">
+                    <div className="accordion__content-body">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Accordion
