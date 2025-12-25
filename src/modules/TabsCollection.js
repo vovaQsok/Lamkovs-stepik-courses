83c27ac2d1a32ad04738157e307b42a4c030697d
vovaQsok <qraqra88@gmail.com>
2025-12-25 21:56:45 +0300

    step(s) 3.11 Section Plans — JS for Tabs and TabsNavigation

--- src/modules/TabsCollection.js
+++ src/modules/TabsCollection.js
@@ -1,155 +1,5 @@
-// import getParams from "@/utils/getParams";
-//
-// const rootSelector = '[data-js-tabs]'
-//
-// class Tabs {
-//     selectors = {
-//         root: rootSelector,
-//         navigation: '[data-js-tabs-navigation]',
-//         button: '[data-js-tabs-button]',
-//         content:'[data-js-tabs-content]',
-//     }
-//
-//     stateClasses = {
-//         isActive: 'is-active',
-//     }
-//
-//     stateCSSVariables = {
-//         activeButtonWidth: '--tabsActiveButtonWidth',
-//         activeButtonOffsetLeft: '--tabsActiveButtonOffsetLeft',
-//     }
-//
-//     constructor(rootElement) {
-//         this.rootElement = rootElement
-//         this.params = getParams(this.rootElement, this.selectors.root)
-//         this.navigationElement = this.params.navigationTargetElementId
-//         ? document.getElementById(this.params.navigationTargetElementId)
-//             : this.rootElement.querySelector(this.selectors.navigation)
-//         this.buttonElements = [...this.navigationElement.querySelectorAll(this.selectors.button)]
-//         this.contentElements = [...this.rootElement.querySelectorAll(this.selectors.content)]
-//         this.state = {
-//             activeTabIndex: this.buttonElements.findIndex(( ariaSelected ) => { ariaSelected })
-//         }
-//         this.limitTabsIndex = this.buttonElements.length - 1
-//         this.bindEvents()
-//
-//     }
-//
-//     updateUI() {
-//         const { activeTabIndex } = this.state
-//
-//         this.buttonElements.forEach((buttonElement, index) => {
-//             const isActive = index === activeTabIndex
-//
-//             buttonElement.classList.toggle(this.stateClasses.isActive, isActive)
-//             buttonElement.ariaSelected = isActive
-//             buttonElement.tabIndex = isActive ? 0 : -1
-//         })
-//
-//         this.contentElements.forEach((contentElement, index) => {
-//            const isActive = index === activeTabIndex
-//
-//            contentElement.classList.toggle(this.stateClasses.isActive, isActive)
-//         })
-//     }
-//
-//     activateTab(newTabIndex) {
-//         this.state.activeTabIndex = newTabIndex
-//         this.updateUI()
-//         this.buttonElements[newTabIndex].focus()
-//     }
-//
-//     previousTab = () => {
-//         const newTabIndex = this.activeTabIndex === 0
-//         ? this.limitTabsIndex
-//         : this.state.activeTabIndex - 1
-//
-//         this.activateTab(newTabIndex)
-//     }
-//
-//     nextTab = () => {
-//         const newTabIndex = this.state.activeTabIndex === this.limitTabsIndex
-//             ? 0 : this.state.activeTabIndex - 1
-//
-//         this.activateTab(newTabIndex)
-//     }
-//
-//     firstTab = () => {
-//         this.activateTab(0)
-//     }
-//
-//     lastTab = () => {
-//         this.activateTab(this.limitTabsIndex)
-//     }
-//
-//     onButtonClick(buttonIndex) {
-//         this.state.activeTabIndex = buttonIndex
-//         this.updateUI()
-//     }
-//
-//     onKeyDown = (event) => {
-//         const { target } = event
-//         const isTabsContentFocused = this.contentElements
-//             .some((contentElement) => contentElement === target)
-//         const isTabsButtonFocused = this.buttonElements
-//             .some((buttonElement) => buttonElement === target)
-//
-//         if (!isTabsContentFocused && !isTabsButtonFocused) {
-//             return
-//         }
-//
-//         const action = {
-//             ArrowLeft: this.previousTab,
-//             ArrowRight: this.nextTab,
-//             Home: this.firstTab,
-//             End: this.lastTab,
-//         }[code]
-//
-//         const isMacHomeKey = metaKey && code === "ArrowLeft"
-//         if (isMacHomeKey) {
-//             event.preventDefault()
-//             this.firstTab()
-//             return
-//         }
-//
-//         const isMacEndKey = metaKey && code === "ArrowRight"
-//         if (isMacEndKey) {
-//             event.preventDefault()
-//             this.lastTab()
-//             return
-//         }
-//
-//         if (action) {
-//             event.preventDefault()
-//             action()
-//         }
-//     }
-//
-//     bindEvents() {
-//         this.buttonElements.forEach((buttonElement, index) => {
-//             buttonElement.addEventListener('click', () => this.onButtonClick(index))
-//         })
-//
-//         document.addEventListener('keydown', this.onKeyDown)
-//     }
-// }
-//
-// class TabsCollection {
-//     constructor() {
-//         this.init()
-//     }
-//
-//     init() {
-//         document.querySelectorAll(rootSelector).forEach((element) => {
-//             new Tabs(element)
-//         })
-//     }
-// }
-//
-// export default TabsCollection
-
-import getParams from '@/utils/getParams'
-import pxToRem from '@/utils/pxToRem'
+import getParams from "@/utils/getParams";
+import pxToRem from "@/utils/pxToRem";
 
 const rootSelector = '[data-js-tabs]'
 
@@ -158,7 +8,7 @@ class Tabs {
         root: rootSelector,
         navigation: '[data-js-tabs-navigation]',
         button: '[data-js-tabs-button]',
-        content: '[data-js-tabs-content]',
+        content:'[data-js-tabs-content]',
     }
 
     stateClasses = {
@@ -174,12 +24,12 @@ class Tabs {
         this.rootElement = rootElement
         this.params = getParams(this.rootElement, this.selectors.root)
         this.navigationElement = this.params.navigationTargetElementId
-            ? document.getElementById(this.params.navigationTargetElementId)
+        ? document.getElementById(this.params.navigationTargetElementId)
             : this.rootElement.querySelector(this.selectors.navigation)
         this.buttonElements = [...this.navigationElement.querySelectorAll(this.selectors.button)]
         this.contentElements = [...this.rootElement.querySelectorAll(this.selectors.content)]
         this.state = {
-            activeTabIndex: this.buttonElements.findIndex(({ ariaSelected }) => ariaSelected)
+            activeTabIndex: this.buttonElements.findIndex(( {ariaSelected} ) =>  ariaSelected )
         }
         this.limitTabsIndex = this.buttonElements.length - 1
         this.bindEvents()
@@ -196,22 +46,23 @@ class Tabs {
             buttonElement.ariaSelected = isActive
             buttonElement.tabIndex = isActive ? 0 : -1
 
-            if (isActive) {
+            if (isActive)
+            {
                 this.updateNavigationCSSVars(buttonElement)
             }
         })
 
         this.contentElements.forEach((contentElement, index) => {
-            const isActive = index === activeTabIndex
+           const isActive = index === activeTabIndex
 
-            contentElement.classList.toggle(this.stateClasses.isActive, isActive)
+           contentElement.classList.toggle(this.stateClasses.isActive, isActive)
         })
     }
 
     updateNavigationCSSVars(
         activeButtonElement = this.buttonElements[this.state.activeTabIndex]
     ) {
-        const { width, left } = activeButtonElement.getBoundingClientRect()
+        const { width, left} = activeButtonElement.getBoundingClientRect()
         const offsetLeft = left - this.navigationElement.getBoundingClientRect().left
 
         this.navigationElement.style.setProperty(
@@ -233,16 +84,15 @@ class Tabs {
 
     previousTab = () => {
         const newTabIndex = this.state.activeTabIndex === 0
-            ? this.limitTabsIndex
-            : this.state.activeTabIndex - 1
+        ? this.limitTabsIndex
+        : this.state.activeTabIndex - 1
 
         this.activateTab(newTabIndex)
     }
 
     nextTab = () => {
         const newTabIndex = this.state.activeTabIndex === this.limitTabsIndex
-            ? 0
-            : this.state.activeTabIndex + 1
+            ? 0 : this.state.activeTabIndex + 1
 
         this.activateTab(newTabIndex)
     }
@@ -278,14 +128,14 @@ class Tabs {
             End: this.lastTab,
         }[code]
 
-        const isMacHomeKey = metaKey && code === 'ArrowLeft'
+        const isMacHomeKey = metaKey && code === "ArrowLeft"
         if (isMacHomeKey) {
             event.preventDefault()
             this.firstTab()
             return
         }
 
-        const isMacEndKey = metaKey && code === 'ArrowRight'
+        const isMacEndKey = metaKey && code === "ArrowRight"
         if (isMacEndKey) {
             event.preventDefault()
             this.lastTab()
@@ -302,6 +152,7 @@ class Tabs {
         this.buttonElements.forEach((buttonElement, index) => {
             buttonElement.addEventListener('click', () => this.onButtonClick(index))
         })
+
         document.addEventListener('keydown', this.onKeyDown)
     }
 
@@ -329,3 +180,187 @@ class TabsCollection {
 }
 
 export default TabsCollection
+
+// //////////////////////////////////////////////////////////////////
+//
+// import getParams from '@/utils/getParams'
+// import pxToRem from '@/utils/pxToRem'
+//
+// const rootSelector = '[data-js-tabs]'
+//
+// class Tabs {
+//     selectors = {
+//         root: rootSelector,
+//         navigation: '[data-js-tabs-navigation]',
+//         button: '[data-js-tabs-button]',
+//         content: '[data-js-tabs-content]',
+//     }
+//
+//     stateClasses = {
+//         isActive: 'is-active',
+//     }
+//
+//     stateCSSVariables = {
+//         activeButtonWidth: '--tabsNavigationActiveButtonWidth',
+//         activeButtonOffsetLeft: '--tabsNavigationActiveButtonOffsetLeft',
+//     }
+//
+//     constructor(rootElement) {
+//         this.rootElement = rootElement
+//         this.params = getParams(this.rootElement, this.selectors.root)
+//         this.navigationElement = this.params.navigationTargetElementId
+//             ? document.getElementById(this.params.navigationTargetElementId)
+//             : this.rootElement.querySelector(this.selectors.navigation)
+//         this.buttonElements = [...this.navigationElement.querySelectorAll(this.selectors.button)]
+//         this.contentElements = [...this.rootElement.querySelectorAll(this.selectors.content)]
+//         this.state = {
+//             activeTabIndex: this.buttonElements.findIndex(({ ariaSelected }) => ariaSelected)
+//         }
+//         this.limitTabsIndex = this.buttonElements.length - 1
+//         this.bindEvents()
+//         setTimeout(this.bindObservers, 500)
+//     }
+//
+//     updateUI() {
+//         const { activeTabIndex } = this.state
+//
+//         this.buttonElements.forEach((buttonElement, index) => {
+//             const isActive = index === activeTabIndex
+//
+//             buttonElement.classList.toggle(this.stateClasses.isActive, isActive)
+//             buttonElement.ariaSelected = isActive
+//             buttonElement.tabIndex = isActive ? 0 : -1
+//
+//             if (isActive) {
+//                 this.updateNavigationCSSVars(buttonElement)
+//             }
+//         })
+//
+//         this.contentElements.forEach((contentElement, index) => {
+//             const isActive = index === activeTabIndex
+//
+//             contentElement.classList.toggle(this.stateClasses.isActive, isActive)
+//         })
+//     }
+//
+//     updateNavigationCSSVars(
+//         activeButtonElement = this.buttonElements[this.state.activeTabIndex]
+//     ) {
+//         const { width, left } = activeButtonElement.getBoundingClientRect()
+//         const offsetLeft = left - this.navigationElement.getBoundingClientRect().left
+//
+//         this.navigationElement.style.setProperty(
+//             this.stateCSSVariables.activeButtonWidth,
+//             `${pxToRem(width)}rem`
+//         )
+//
+//         this.navigationElement.style.setProperty(
+//             this.stateCSSVariables.activeButtonOffsetLeft,
+//             `${pxToRem(offsetLeft)}rem`
+//         )
+//     }
+//
+//     activateTab(newTabIndex) {
+//         this.state.activeTabIndex = newTabIndex
+//         this.updateUI()
+//         this.buttonElements[newTabIndex].focus()
+//     }
+//
+//     previousTab = () => {
+//         const newTabIndex = this.state.activeTabIndex === 0
+//             ? this.limitTabsIndex
+//             : this.state.activeTabIndex - 1
+//
+//         this.activateTab(newTabIndex)
+//     }
+//
+//     nextTab = () => {
+//         const newTabIndex = this.state.activeTabIndex === this.limitTabsIndex
+//             ? 0
+//             : this.state.activeTabIndex + 1
+//
+//         this.activateTab(newTabIndex)
+//     }
+//
+//     firstTab = () => {
+//         this.activateTab(0)
+//     }
+//
+//     lastTab = () => {
+//         this.activateTab(this.limitTabsIndex)
+//     }
+//
+//     onButtonClick(buttonIndex) {
+//         this.state.activeTabIndex = buttonIndex
+//         this.updateUI()
+//     }
+//
+//     onKeyDown = (event) => {
+//         const { target, code, metaKey } = event
+//         const isTabsContentFocused = this.contentElements
+//             .some((contentElement) => contentElement === target)
+//         const isTabsButtonFocused = this.buttonElements
+//             .some((buttonElement) => buttonElement === target)
+//
+//         if (!isTabsContentFocused && !isTabsButtonFocused) {
+//             return
+//         }
+//
+//         const action = {
+//             ArrowLeft: this.previousTab,
+//             ArrowRight: this.nextTab,
+//             Home: this.firstTab,
+//             End: this.lastTab,
+//         }[code]
+//
+//         const isMacHomeKey = metaKey && code === 'ArrowLeft'
+//         if (isMacHomeKey) {
+//             event.preventDefault()
+//             this.firstTab()
+//             return
+//         }
+//
+//         const isMacEndKey = metaKey && code === 'ArrowRight'
+//         if (isMacEndKey) {
+//             event.preventDefault()
+//             this.lastTab()
+//             return
+//         }
+//
+//         if (action) {
+//             event.preventDefault()
+//             action()
+//         }
+//     }
+//
+//     bindEvents() {
+//         this.buttonElements.forEach((buttonElement, index) => {
+//             buttonElement.addEventListener('click', () => this.onButtonClick(index))
+//         })
+//         document.addEventListener('keydown', this.onKeyDown)
+//     }
+//
+//     onResize = () => {
+//         this.updateNavigationCSSVars()
+//     }
+//
+//     bindObservers = () => {
+//         const resizeObserver = new ResizeObserver(this.onResize)
+//
+//         resizeObserver.observe(this.navigationElement)
+//     }
+// }
+//
+// class TabsCollection {
+//     constructor() {
+//         this.init()
+//     }
+//
+//     init() {
+//         document.querySelectorAll(rootSelector).forEach((element) => {
+//             new Tabs(element)
+//         })
+//     }
+// }
+//
+// export default TabsCollection
--- src/sections/Plans/plansGroups.js
+++ src/sections/Plans/plansGroups.js
@@ -58,35 +58,6 @@ const planGroups = [
             },
         ]
     },
-    {
-        title: 'BLABLABLYA',
-        items: [
-            {
-                title: 'Basic Plan',
-                description: 'Enjoy an extensive library of movies and shows, featuring a range of content, including recently released titles.',
-                price: {
-                    value: '$99.99',
-                    label: '/year',
-                },
-            },
-            {
-                title: 'Standard Plan',
-                description: 'Access to a wider selection of movies and shows, including most new releases and exclusive content',
-                price: {
-                    value: '$129.99',
-                    label: '/year',
-                },
-            },
-            {
-                title: 'Premium Plan',
-                description: 'Access to a widest selection of movies and shows, including all new releases and Offline Viewing',
-                price: {
-                    value: '$149.99',
-                    label: '/year',
-                },
-            },
-        ]
-    }
 ]
 
 export default planGroups
